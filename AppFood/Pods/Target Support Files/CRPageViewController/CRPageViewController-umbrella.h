41c7f6885639a8f27b7f08229b00b8489dbf53c1
HuyHoang <hhoangna@gmail.com>
2018-04-08 11:22:31 +0700

    Update

---
+++ AppFood/Pods/Target Support Files/CRPageViewController/CRPageViewController-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "CRPageViewController.h"
+
+FOUNDATION_EXPORT double CRPageViewControllerVersionNumber;
+FOUNDATION_EXPORT const unsigned char CRPageViewControllerVersionString[];
+
